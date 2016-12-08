2c10a7a13c03de3c2374459234c1a663502a26e0
mwagyuvm <mark.wagy@gmail.com>
2016-12-08 14:41:28 -0500

    multiple updates. higher and higher-level functions.

--- js/parsercombinator.js
+++ js/parsercombinator.js
@@ -6,6 +6,10 @@ const PEG = require('pegjs');
 
 const VERBOSE = true;
 
+function log(msg) {
+    if (VERBOSE) console.log(msg);
+}
+
 PC_DEF_GRAMMAR = `
 
 start = expression+
@@ -54,9 +58,8 @@ class ParserCombinator {
 	}
 
 	// this returns a function that matches or not according to this parser combinator instance
-	matchLambda(sequence) {
-	    // TODO
-        return;
+    getMatchLambda() {
+        return this.matchLambda;
     }
 
     inputAdvance() {
@@ -80,12 +83,16 @@ class ParserCombinator {
     }
 
 	buildFromPEGDefinition(pcDef) {
-        // this is where we'll create the actual parsers to use in the combinator
-        /*
-        this.parser = this.or(pcDef.map((pattern) => {
-            this.matchSequence(pattern);
-        }));
-        */
+        // TODO construct the appropriate parser combinator based on the pcDef
+        this.matchLambda = function (x,y) {
+            return pc.matchSequence(
+                pc.matchOr(
+                    pc.matchSingle(x),
+                    pc.matchSingle(y)
+                ),
+                pc.matchSingle(x)
+            )
+        };
     }
 
     matchSingle(c) {
@@ -94,10 +101,10 @@ class ParserCombinator {
             const r = self.inputRead(input);
             if (r == c) {
                 self.inputAdvance();
-                if (VERBOSE) console.log(`    ${r} == ${c} ? Y`);
+                log(`    ${r}(i) == ${c} ? Y`);
                 return c;
             } else {
-                if (VERBOSE) console.log(`    ${r} == ${c} ? N`);
+                log(`    ${r}(i) == ${c} ? N`);
                 return self.failure;
             }
         });
@@ -106,7 +113,7 @@ class ParserCombinator {
     matchOr(...parsers) {
 	    let self = this;
         return (function (input) {
-            if (VERBOSE) console.log("   or {");
+            log("   or {");
             const results = parsers.map( (parser) => {
                 return parser(input);
             }).filter( (result) => {
@@ -114,11 +121,11 @@ class ParserCombinator {
             });
             if (results.length > 0) {
                 // arbitrarily choosing the first matched or element.
-                // TODO rethink this
-                if (VERBOSE) console.log("   } or");
+                // TODO: think this over
+                log("   } or");
                 return results[0];
             } else {
-                if (VERBOSE) console.log("   } or");
+                log("   } or");
                 return self.failure;
             }
         });
@@ -127,10 +134,10 @@ class ParserCombinator {
     matchSequence(...parsers) {
 	    let self = this;
         return (function (input) {
-            if (VERBOSE) console.log("   seq {");
+            log("   seq {");
             const pos = self.inputGetIndex();
             if (pos > input.length) {
-                if (VERBOSE) console.log("   } seq");
+                log("   } seq");
                 return self.failure;
             }
             const results = parsers.map( (parser) => {
@@ -145,121 +152,18 @@ class ParserCombinator {
                     return x !== self.failure;
                 })) {
                 self.inputSetIndex(pos);
-                if (VERBOSE) console.log("   } seq");
+                log("   } seq");
                 return self.failure;
             }
-            if (VERBOSE) console.log("   } seq");
+            log("   } seq");
             return results;
         });
     }
 
 }
 
-/*
-const seq = "babc";
-let idx = 0;
-let failure = false;
-
-function inputRead(input) {
-    return input[idx];
-}
-
-function inputAdvance(input, howmuch) {
-    idx += howmuch;
-}
-
-function inputReset() {
-    idx = 0;
-}
-
-function inputGet() {
-    return idx;
-}
-
-function inputSet(val) {
-    idx = val;
-}
-
-
-let singleMatch = function(c) {
-
-    return (function(input) {
-        const r = inputRead(input);
-        if (r == c) {
-            inputAdvance(input, 1);
-            return c;
-        } else {
-            return failure;
-        }
-    });
-
-};
-
-
-function or(...parsers) {
-
-    return (function(input) {
-        const results = parsers.map( (parser) => {
-            return parser(input);
-        }).filter( (result) => {
-            return result !== false;
-        });
-        if (results.length > 0) {
-            return results;
-        } else {
-            return false;
-        }
-    });
-
-}
-
-
-function seqmatch(...parsers) {
-
-    return (function (input) {
-        const pos = inputGet(input);
-        if (pos > input.length) {
-            return false;
-        }
-        const results = parsers.map( (parser) => {
-            let result = parser(input);
-            if (result === false) { inputSet(pos); return false; }
-            return result;
-        });
-        if (!results.every( (x) => { return x !== false; })) {
-            inputSet(pos);
-            return false;
-        }
-        return results;
-    });
-
-}
-*/
 
 function main() {
-/*
-	//var parser = singleMatch('a');
-	const parser = or(singleMatch('a'), singleMatch('b'));
-	const result1 = parser(seq);
-    console.log("result1 " +  result1);
-
-    inputReset();
-	const parser2 = seqmatch(singleMatch('a'), singleMatch('b'));
-	const result2 = parser2(seq);
-    console.log("result2 " +  result2);
-
-    inputReset();
-    const parser3 = or(
-    	seqmatch(singleMatch('a'), singleMatch('b')),
-		seqmatch(singleMatch('b'), singleMatch('a')));
-	const result3 = parser3(seq);
-	console.log("result3: " + result3);
-
-	inputReset();
-	const parser4 = seqmatch(singleMatch('b'), singleMatch('a'), singleMatch('b'));
-    const result4 = parser4(seq);
-	console.log("result4: " + result4);
-*/
 
     const pc = new ParserCombinator("a;");
     const parser = pc.matchSequence(
@@ -269,11 +173,28 @@ function main() {
         ),
         pc.matchSingle('c')
     );
-    console.log("ac");
-    console.log(parser("ac"));
+
+    const parserparser = function (x,y) {
+        return pc.matchSequence(
+            pc.matchOr(
+                pc.matchSingle(x),
+                pc.matchSingle(y)
+            ),
+        pc.matchSingle(x)
+        )
+    };
+    console.log("a|b c;");
+    console.log(parser("bd"));
+
     pc.inputResetIndex();
-    console.log("ba");
-    console.log(parser("ba"));
+    console.log("PARSERPARSER");
+    console.log("a|b a;");
+    const pp = parserparser('a','b');
+    const res = pp("bd");
+    console.log(res);
+    //pc.inputResetIndex();
+    //console.log("ba");
+    //console.log(parser("ba")('a','b','z'));
 
 }
 
