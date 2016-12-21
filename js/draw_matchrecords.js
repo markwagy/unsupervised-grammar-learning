cb192cacec9777f5d689795b1252bf3dbb70b89b
mwagyuvm <mark.wagy@gmail.com>
2016-12-21 11:53:32 -0500

    document-wide iteration selection

--- js/draw_matchrecords.js
+++ js/draw_matchrecords.js
@@ -16,7 +16,10 @@ function loadMatchRecords() {
 
 		data.sort( (a, b) => { return b.counts - a.counts; });
 
-		d3.select("table#matchrecords").selectAll("tr").data(data).enter().append("tr")
+		// ignore match records that don't have a count of at least 2
+		let datafilt = data.filter(d => { return d.counts > 1; });
+
+		d3.select("table#matchrecords").selectAll("tr").data(datafilt).enter().append("tr")
 			.html( (d) => {
 				return cols.reduce( (p, c) => {
 					return p + `<td>${d[c]}</td>`;
