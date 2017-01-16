0602b17779bd40e33502e800cbde98eb7c403b03
mwagyuvm <mark.wagy@gmail.com>
2017-01-16 15:13:07 -0500

    change matcher to use manual pattern matching so we can have a more powerful match language

--- js/matcher.js
+++ js/matcher.js
@@ -21,6 +21,156 @@ class MatchObj {
 
 }
 
+class Stream {
+
+    constructor(vals) {
+        this.i = 0;
+        this.vals = vals;
+    }
+
+    next() {
+        return this.vals[this.i+1];
+    }
+
+    hasNext() {
+        return this.vals.length > (this.i+1);
+    }
+
+    getIndex() {
+        return this.i;
+    }
+
+}
+
+
+class TokenDict {
+
+    constructor() {
+        this.d = {};
+    }
+
+    keys() {
+        return Object.keys(this.d);
+    }
+
+    init(t) {
+        this.d[t] = null;
+    }
+
+    numKeys() {
+        return this.keys().length;
+    }
+
+    values() {
+        let v = this.keys().map(x => { return this.d[x]}).filter( x => { return x !== null; });
+        return v;
+    }
+
+    hasKey(k) {
+        return this.keys().indexOf(k) >= 0;
+    }
+
+    hasValue(v) {
+        return this.values().indexOf(v) >= 0;
+    }
+
+    add(k, v="EMPTY") {
+        if (this.hasKey(k)) {
+            if (this.d[k] === null) {
+                this.d[k] = v;
+            } else if (v !== this.d[k]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    get(k) {
+        return this.d[k];
+    }
+
+    isNull(key) {
+        return this.d[key] === null;
+    }
+
+    clear() {
+        this.d = {};
+    }
+
+}
+
+
+class MatchFunction {
+
+    constructor(patternString) {
+        const TOKEN_SEP = " ";
+        this.tokenDict = new TokenDict();
+        this.tokens = patternString.split(TOKEN_SEP);
+        this.uniqueTokens = this.getUniqueTokensInOrder(this.tokens);
+        this.uniqueTokens.forEach(t => {this.tokenDict.init(t);});
+    }
+
+    getUniqueTokensInOrder(toks) {
+        return toks.reduce( (p, c) => {
+            if (p.indexOf(c) < 0) {
+                p.push(c);
+            }
+            return p;
+        }, []);
+    }
+
+    getVarFromVal(val) {
+        return this.tokenDict.keys().filter( k => {
+            return this.tokenDict.get(k) === val;
+        })[0];
+    }
+
+    reset() {
+        this.tokenDict.clear();
+        this.uniqueTokens.forEach(t => {this.tokenDict.init(t);});
+    }
+
+    run(seq) {
+        let canBeAMatch = true;
+        let i=-1;
+        let matchedVals = [];
+        while (canBeAMatch) {
+            i++;
+            if (i>=seq.length || i>=this.tokens.length) {
+                canBeAMatch = false;
+                break;
+            }
+            let currentVar = this.tokens[i];
+            let currentVal = seq[i];
+            let varIsAlreadyAssigned = !this.tokenDict.isNull(currentVar);
+            let valAlreadyExists = this.tokenDict.values().indexOf(currentVal) >= 0;
+            if (varIsAlreadyAssigned) {
+                let assignedVal = this.tokenDict.get(currentVar);
+                if (assignedVal !== currentVal) {
+                    canBeAMatch = false;
+                    break;
+                }
+            } else if (valAlreadyExists) {
+                let assignedVar = this.getVarFromVal(currentVal);
+                if (assignedVar !== currentVar) {
+                    canBeAMatch = false;
+                    break;
+                }
+            } else {
+                this.tokenDict.add(currentVar, currentVal);
+            }
+            // if we get here, the current val is a match with the current var
+            matchedVals.push(currentVal);
+            if (matchedVals.length === this.tokens.length) {
+                break;
+            }
+        }
+        this.reset();
+        return canBeAMatch?matchedVals:[];
+    }
+
+}
+
 class Matcher {
 
     constructor(patternString) {
@@ -62,5 +212,16 @@ function test1() {
     log(matcher.match([1]));
 }
 
-test1();
-//module.exports.Matcher = Matcher;
+function test2() {
+    const mf = new MatchFunction("X X Y X");
+    console.log(mf.run([1, 1, 1, 1]));
+    console.log(mf.run([1, 1, 3, 1, 3]));
+    console,log(mf.run(["a","b","c","d"]));
+    console.log(mf.run([0,0,1]));
+    console.log(mf.run([0,1,1,0,1]));
+    console.log(mf.run(["a","a","b","a","a"]))
+}
+
+
+//test2();
+module.exports.Matcher = Matcher;
